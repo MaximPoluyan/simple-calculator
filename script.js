937020554b4b13525fb87d047b35d6b7db14aa80
MaximPoluyan <maxim.poluyan@dspot.com.pl>
2026-01-30 10:49:07 +0300

    add consts and plus logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS подключен и работает!");
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const btnPlus = document.getElementById('btn-plus');
+const btnMinus = document.getElementById('btn-minus');
+const btnMultiply = document.getElementById('btn-multiply');
+const btnDivide = document.getElementById('btn-divide');
+
+const result = document.getElementById('result');
+
+btnPlus.onclick = function() {
+    const sum = Number(num1.value) + Number(num2.value);
+    result.textContent = "Результат: " + sum;
+};
